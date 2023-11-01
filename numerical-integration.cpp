5c402dd8e18bb50ae42ef36f43e732efd89a2c3c
Nathan Hernandez <nathannatedoghernandez@gmail.com>
2023-11-01 18:39:54 -0500

    1.5 atomic done

--- numerical-integration.cpp
+++ numerical-integration.cpp
@@ -10,7 +10,8 @@
 double pi = 0.0;
 int numPoints = 1000000000;
 int numThreads = 4;
-pthread_mutex_t mutex;
+pthread_mutex_t mutex; // used for 1.4
+std::atomic<double> atomic_pi{0.0}; // used for 1.5 (given)
 
 // 1.1 Use your knowledge of basic calculus to explain briefly why this code provides an estimate for pi.
 /*
@@ -61,15 +62,20 @@ double f_area(double x) {
 }
 
 void compute_area_semi_circle() {
-  // int numPoints = 1000000;       // Experiment with different values
-  double step = 1.0 / numPoints; // Width of each subinterval
+  uint64_t execTime; /*time in nanoseconds */
+  struct timespec tick, tock;
+
+  double step_size = 0.5;
+  double step = step_size / numPoints; // Width of each subinterval
   double area = 0.0;
 
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
   for (int i = 0; i < numPoints; i++)
   {
     double x = -0.5 + i * step; // Calculate x within the interval [-0.5, 0.5]
     area += step * f_area(x);        // Add to local sum
   }
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
 
   double actualArea = M_PI / 2.0; // Actual area of the semicircle
 
@@ -79,6 +85,10 @@ void compute_area_semi_circle() {
   // Calculate h for 1% accuracy
   double h = sqrt(24 * (actualArea - area)) / numPoints;
   printf("Step size (h) for 1%% accuracy: %.10f\n", h);
+
+  execTime = 1000000000 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
+
+  printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int)execTime);
 }
 
 // 1.3 Find the running times (of only computing pi) for one, two, four and eight threads and plot the running
@@ -108,11 +118,14 @@ void *computePi(void *arg) {
 
 void test_pthread() {
   // int numPoints = 1000000000; // Total number of points
-  int numThreads = 4;         // Experiment with different thread counts
+  // int numThreads = 4;         // Experiment with different thread counts
+  uint64_t execTime; /*time in nanoseconds */
+  struct timespec tick, tock;
 
   pthread_t threads[numThreads];
   int threadIds[numThreads];
 
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
   for (int i = 0; i < numThreads; i++)
   {
     threadIds[i] = i;
@@ -123,8 +136,12 @@ void test_pthread() {
   {
     pthread_join(threads[i], NULL);
   }
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
 
   printf("Estimated value of pi: %.20f\n", pi);
+  execTime = 1000000000 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
+
+  printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int)execTime);
 }
 
 // 1.4
@@ -153,9 +170,13 @@ void *computePi_mutex(void *arg) {
 }
 
 void test_mutex() {
+  uint64_t execTime; /*time in nanoseconds */
+  struct timespec tick, tock;
   pthread_t threads[numThreads];
   int threadIds[numThreads];
   pthread_mutex_init(&mutex, NULL); // Initialize the mutex
+
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
 
   for (int i = 0; i < numThreads; i++) {
     threadIds[i] = i;
@@ -166,31 +187,95 @@ void test_mutex() {
     pthread_join(threads[i], NULL);
   }
 
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
+
   printf("Estimated value of pi with %d threads: %.20f\n", numThreads, pi);
+
+  execTime = 1000000000 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
+
+  printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int)execTime);
 
   pthread_mutex_destroy(&mutex); // Clean up the mutex
 }
 
 
+//1.5
+void add_to_pi(double bar)
+{
+  auto current = atomic_pi.load();
+  while (!atomic_pi.compare_exchange_weak(current, current + bar))
+    ;
+}
 
+void *computePi_atomic(void *arg)
+{
+  int threadId = *(int *)arg;
+  double step = 0.5 / numPoints;
+  double localSum = 0.0;
 
+  for (int i = threadId; i < numPoints; i += numThreads)
+  {
+    double x = i * step;
+    localSum += step * (6.0 / sqrt(1 - x * x));
+  }
+
+  // Lock the mutex before updating pi
+  // pthread_mutex_lock(&mutex);
+  add_to_pi(localSum);              // Add local sum to global pi
+  // pthread_mutex_unlock(&mutex); // Unlock the mutex
+
+  return NULL;
+}
+void test_atomic()
+{
+  uint64_t execTime; /*time in nanoseconds */
+  struct timespec tick, tock;
+  pthread_t threads[numThreads];
+  int threadIds[numThreads];
+
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
+  for (int i = 0; i < numThreads; i++)
+  {
+    threadIds[i] = i;
+    pthread_create(&threads[i], NULL, computePi_atomic, &threadIds[i]);
+  }
+
+  for (int i = 0; i < numThreads; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
+
+  execTime = 1000000000 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
+  printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int)execTime);
+  printf("Estimated value of pi with %d threads: %.20f\n", numThreads, pi);
+}
 
 int main(int argc, char *argv[]) {
   //1.1
+  printf("----------------------STARTING TEST 1.1 ---------------------- \n\n");
   sequential_program();
+  // printf("----------------------ENDING TEST 1.1 ---------------------- \n\n");
 
   // 1.2
+  printf("----------------------STARTING TEST 1.2 ---------------------- \n\n");
   compute_area_semi_circle();
+  // printf("----------------------ENDING TEST 1.2 ---------------------- \n\n");
 
   // 1.3
   pi = 0;
+  printf("----------------------STARTING TEST 1.3 ---------------------- \n\n");
   test_pthread();
+  // printf("----------------------ENDING TEST 1.3 ---------------------- \n\n");
 
   // 1.4
   pi = 0;
+  printf("----------------------STARTING TEST 1.4 ---------------------- \n\n");
   test_mutex();
+  // printf("----------------------ENDING TEST 1.4 ---------------------- \n\n");
 
   // 1.5
+  printf("----------------------STARTING TEST 1.5 ---------------------- \n\n");
+  test_atomic();
 
   // 1.6
 
